d4054e17f130059296d372cc409d4c3b58b2818e
Ekaterina Yurkovskaya <72210172+Kat-ies@users.noreply.github.com>
2021-02-16 00:56:22 +0300

    add docstring

--- src/zips_from_google_drive.py
+++ src/zips_from_google_drive.py
@@ -1,27 +1,28 @@
+""""
+functions:
+drive_connect()
+unpacking_zip()
+"""
+
 import zipfile
 from google.colab import drive
 
-directories = ['/content/drive/My Drive/КУ Курсачи/Курсовой проект 2020/WIDER_FACE (zip)/WIDER_train.zip']
+directories = ['/content/drive/My Drive/КУ Курсачи/'
+               'Курсовой проект 2020/WIDER_FACE (zip)/WIDER_train.zip']
 
-
-# function drive_connect connects to google-drive
 
 def drive_connect():
+    """function drive_connect() connects to google-drive"""
     drive.mount('/content/drive')
 
 
-# function unpacking_zip unpacks zip files from the list of directories
-# now we need only one archive, when we need more, we'll add new paths
-# to the list and easily unpack all archives at once
-
 def unpacking_zip():
-    # распакуем архивы из гугл диска
-    z = []
+    """
+    function unpacking_zip() unpacks zip files from the list of directories
+    now we need only one archive, when we need more, we'll add new paths
+    to the list and easily unpack all archives at once"""
     for zips in directories:
-        z.append(zipfile.ZipFile(zips, 'r'))
-
-    for archives in z:
-        archives.extractall()
+        (zipfile.ZipFile(zips, 'r')).extractall()
 
 
 if __name__ == '__main__':
