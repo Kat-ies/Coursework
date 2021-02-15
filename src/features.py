88e5a79320562823b5ef1247e4c9fe79a67ab8e6
Ekaterina Yurkovskaya <72210172+Kat-ies@users.noreply.github.com>
2021-02-15 18:11:16 +0300

    updated .py files

--- src/features.py
+++ src/features.py
@@ -1,26 +1,33 @@
 from lxml import etree
+import sklearn.model_selection as sk
 from collections import namedtuple
+import pandas as pd
+import time
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
 import os
+import random
 
 MAXSIZE = (25, 25)
 
 # class HaarFeature is created for calculating the value of every feature
-#
-Представленный выше фрагмент описывает 1 признак Хоара, где прямоугольники задаются так:
-<_> x1, y1, w, h, weight </_>
-(верхний левый угол + ширина + высота + вес)
+# each haars feature is represented by 2-3 rectangles
+# one rectangle looks like <_> x1, y1, w, h, weight </_>
+# (top left corner + width + height + weight)
+# for more details see https://api-2d3d-cad.com/viola-jones-method/
 
-более детальную информацию можно найти здесь:)
-https://api-2d3d-cad.com/viola-jones-method/
+# the example from .xml file (one feature):
+# <features>
+#  <_>
+#       <rects>
+#         <_>
+#            6 4 12 9 -1.</_>
+#         <_>
+#            6 7 12 3 3.</_></rects></_>
+# </features>
 
-<features>
-  <_>
-        <rects>
-          <_>
-            6 4 12 9 -1.</_>
-          <_>
-            6 7 12 3 3.</_></rects></_>
-</features>
 
 class HaarFeature:
 
@@ -45,6 +52,8 @@ class HaarFeature:
 PATH = '/content/drive/My Drive/КУ Курсачи/Курсовой проект 2020/files/'
 HaarRect = namedtuple('HaarRect', 'x y w h weight')
 
+# function parse_file reads an input .xml file
+# and returns list with HaarRect
 
 def parse_file():
     h_features = []
@@ -63,6 +72,100 @@ def parse_file():
 
                 h_features.append(HaarFeature(rectangles))
     return h_features
+
+
+RANDOM_SEED = 42
+
+# function split_data separates input data
+# into train and test samples
+
+
+def split_data(faces_dataset):
+    random.seed(RANDOM_SEED)
+    random.shuffle(faces_dataset)
+
+    y = np.array(list(map(lambda x: int(x.is_face), faces_dataset)))
+    x_train, x_test, y_train, y_test = sk.train_test_split(faces_dataset, y, test_size=0.25, random_state=0)
+    return x_train, x_test, y_train, y_test
+
+
+# function feature_creating creates haars and matrix features
+
+def feature_creating(sample, feature_type, h_features):
+    features = []
+
+    for images in sample:
+        img = np.array(images.img, 'uint8')
+        if feature_type != 'hf':
+            features.append(img.flatten())
+        else:
+            # integral representation of the image
+            matrix = np.cumsum(np.cumsum(img, axis=0), axis=1)
+            f_for_one_img = []
+            for fichi in h_features:
+                f_for_one_img.append(fichi.cacl_feature(matrix))
+            features.append(f_for_one_img)
+    return np.array(features)
+
+
+col_list = ['logreg_', 'tree_', 'knn_', 'svm_', 'randforest_', 'ada_boost_', 'grad_boost_']
+categories = ['hf_train', 'mf_train', 'pca_train', 'hf_test', 'mf_test', 'pca_test']
+PATH = '/content/drive/My Drive/КУ Курсачи/Курсовой проект 2020/files/'
+
+
+# function save_data saves all created data
+def save_data(all_features, x_train, x_test, y_test, y_train):
+    joblib.dump(all_features, os.path.join(PATH, 'all_features.pkl'))
+    joblib.dump(y_train, os.path.join(PATH, 'y_train.pkl'))
+    joblib.dump(y_test, os.path.join(PATH, 'y_test.pkl'))
+    joblib.dump(x_train, os.path.join(PATH, 'x_train.pkl'))
+    joblib.dump(x_test, os.path.join(PATH, 'x_test.pkl'))
+
+#
+
+
+def final_data_preprocessing(faces_dataset, time_df):
+    all_features = []
+    h_features = parse_file()
+    x_train, x_test, y_train, y_test = split_data(faces_dataset)
+    samples = [x_train, x_test]
+
+    pca = PCA(n_components=len(h_features))
+    scaler = StandardScaler()
+
+    for i, f_samples in enumerate(categories):
+        parsed_str = f_samples.split("_")
+        if parsed_str[1] == 'train':
+            index = 0
+        else:
+            index = 1
+
+        t0 = time.time()
+        all_features.append(feature_creating(samples[index], parsed_str[0], h_features))
+        t = time.time()
+        time_df.loc[categories[i]] = (t - t0) / len(samples[index])
+
+    # scale new features
+    for i in range(0, len(all_features) // 2 - 1):
+        all_features[i] = scaler.fit_transform(all_features[i])
+        all_features[i + 3] = scaler.transform(all_features[i + 3])
+    all_features[2] = pca.fit_transform(all_features[2])
+    all_features[5] = pca.transform(all_features[5])
+    save_data(all_features, x_train, x_test, y_test, y_train)
+
+
+# время на создание фичей нам ещё пригодится, поэтому давайте пока сохраним куда-нибудь одну колонку
+def save_time(time_df):
+    time_of_features_creat = time_df[col_list[0]].values.tolist()
+    joblib.dump(time_of_features_creat, os.path.join(PATH, 'time_of_features_creat.pkl'))
+    joblib.dump(time_df, os.path.join(PATH, 'Dataframes', 'time_df_part0.pkl'))
+
+
+def create_features_and_return_time(faces_dataset):
+    time_df = pd.DataFrame(index=categories, columns=col_list)
+    final_data_preprocessing(faces_dataset, time_df)
+    save_time(time_df)
+    return time_df
 
 
 if __name__ == '__main__':
