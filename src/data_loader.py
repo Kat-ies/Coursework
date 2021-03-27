e5770fe01dba9554480aa309078674333b8888b3
Ekaterina Yurkovskaya <72210172+Kat-ies@users.noreply.github.com>
2021-03-28 01:17:55 +0300

    маленький временный "костыль"

--- src/data_loader.py
+++ src/data_loader.py
@@ -16,8 +16,24 @@ from constants import *
 from pandas import read_csv
 import requests
 import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from constants import *
-from face_detection.retrain_model import *
+# from face_detection.retrain_model import get_object_detection_model
+# строка 20 у меня почему-то не работает:( Только если так:
+
+def get_object_detection_model():
+    # load an object detection model pre-trained on COCO
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+    # replace the classifier with a new one, that has num_classes which is user-defined
+    num_classes = 2  # 1 + background
+
+    # get the number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    return model
 
 
 def unpacking_zips():
